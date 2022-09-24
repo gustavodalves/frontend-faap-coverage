beb57894bf55391b5918f69671c85209c5265b88
Gustavo Duarte Alves <gustavoduartealves@MacBook-Pro-de-Gustavo.local>
2022-09-24 00:58:48 -0300

    add products

--- src/pages/Products/index.tsx
+++ src/pages/Products/index.tsx
@@ -1,8 +1,36 @@
 import React from 'react';
+import Container from 'react-bootstrap/Container';
+
+import carImage from '../../assets/car.png'
+import { Description, ProductsContainer, Title } from './styled';
+
+const products = [
+    {
+        title: 'Seguro de carro',
+        image: carImage,
+        description: 
+            'sse site possui algumas das mais belas imagens disponíveis na web. Todas as imagens são completamente livres de qualquer tipo de direito autoral.'
+    },
+]
 
 const Products = () => {
+    console.log(products)
     return(
-        <> s </>
+        <>
+            <Container className='container-fluid' style={{paddingTop: '80px', paddingBottom: '50px'}}>
+                <h1 className='text-center'> Produtos </h1>
+                {
+                    products.map(product => (
+                        <ProductsContainer>
+                            <Title> { product.title }</Title>
+                            <img className='img-fluid' src={product.image} />
+                            <Description> {product.description}</Description>
+                        </ProductsContainer>
+                    ))
+                }
+            </Container>
+        </>
+            
     )
 }
 
---
+++ src/pages/Products/styled.ts
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const ProductsContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    margin-top: 30px;
+`
+
+export const Title = styled.h2`
+    color: #123680;
+`
+
+export const Description = styled.p`
+    margin-top: 10px;
+`
